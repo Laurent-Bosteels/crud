68a07980cd53b5eb366e0c23d7fefb28d9c8b174
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-06-21 10:21:30 +0200

    created pages + controllers

--- View/includes/header.php
+++ View/includes/header.php
@@ -5,9 +5,32 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Becode - Boiler plate MVC</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    <title>Crud</title>
 </head>
 <body>
     <header>
-        <h1>Welcome to my site</h1>
+    <nav class="navbar is-primary">
+    <!-- logo / brand -->
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="..." style="max-height: 50px"
+          class="py-2 px-2">
+      </a>
+      <a class="navbar-burger" id="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+    </div>
+
+    <div class="navbar-menu" id="nav-links">
+      <!-- right links -->
+      <div class="navbar-end">
+        <a class="navbar-item" href="index.php?page=students">Students</a>
+        <a class="navbar-item" href="index.php?page=teacher">Teacher</a>
+        <a class="navbar-item" href="index.php?page=class">Class</a>
+      </div>
+    </div>
+  </nav>
     </header>
--- index.php
+++ index.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 //include all your model files here
 require 'Model/User.php';
 //include all your controllers here
-require 'Controller/HomepageController.php';
-require 'Controller/InfoController.php';
+require 'Controller/StudentsController.php';
+require 'Controller/TeacherController.php';
+require 'Controller/ClassController.php';
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
 
-$controller = new HomepageController();
-if(isset($_GET['page']) && $_GET['page'] === 'info') {
-    $controller = new InfoController();
+$controller = new StudentsController();
+
+if(isset($_GET['page']) && $_GET['page'] === 'teacher') {
+    $controller = new TeacherController();
 }
 
+if(isset($_GET['page']) && $_GET['page'] === 'class') {
+    $controller = new ClassController();
+}
 
 $controller->render($_GET, $_POST);
